5feaee9bf9fbb59a60a019793bd87de7e9a254bb
danunegru <danielnegru148@gmail.com>
2025-05-22 14:05:58 +0200

    Add feature-buttons component to home-page

---
+++ skate-judge-app/src/app/home-page/buttons-section/buttons-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buttons-section',
+  imports: [],
+  templateUrl: './buttons-section.component.html',
+  styleUrl: './buttons-section.component.scss'
+})
+export class ButtonsSectionComponent {
+
+}
