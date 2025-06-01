306e6437f93b41fbf949eb13b9c13517a6edaf75
vananhluong <luongvananh18@gmail.com>
2025-06-01 12:43:41 +0200

    Add bounce animation to buttons

--- skate-judge-app/src/app/freilaufer/freilaufer.component.spec.ts
+++ skate-judge-app/src/app/freilaufer/freilaufer.component.spec.ts
@@ -21,3 +21,7 @@ describe('FreilauferComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+function beforeEach(arg0: () => Promise<void>) {
+  throw new Error('Function not implemented.');
+}
+
--- skate-judge-app/src/main.ts
+++ skate-judge-app/src/main.ts
@@ -3,7 +3,15 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)],
+  providers: [provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })],
 });
