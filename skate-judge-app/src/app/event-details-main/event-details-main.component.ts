a4a22f12931f375acd789045c14fdca1e0d69406
vananhluong <luongvananh18@gmail.com>
2025-06-16 12:34:53 +0200

    refactor: replace delete button with back navigation
    - Remove delete functionality from event details
    - Add back button with navigation to home
    - Update component styles for back button
    - Clean up optional chaining in template

--- skate-judge-app/src/app/event-details-main/event-details-main.component.ts
+++ skate-judge-app/src/app/event-details-main/event-details-main.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
-import { Event } from '../shared/models/event.interface';
+import { Event, Exam } from '../shared/models/event.interface';
 
 @Component({
   selector: 'app-event-details',
@@ -12,7 +12,8 @@ import { Event } from '../shared/models/event.interface';
   styleUrls: ['./event-details-main.component.scss']
 })
 export class EventDetailsComponent implements OnInit {
-  event: Event | undefined;
+  eventDetails: Event | undefined;
+  selectedExams: Exam[] = [];
 
   constructor(
     private route: ActivatedRoute,
@@ -21,24 +22,19 @@ export class EventDetailsComponent implements OnInit {
 
   ngOnInit() {
     const eventId = this.route.snapshot.paramMap.get('id');
-    console.log('EventDetails: Received ID:', eventId); // Debug log
-
-    if (!eventId) {
-      console.error('No event ID provided');
-      this.router.navigate(['/']);
-      return;
+    if (eventId) {
+      const savedEvents = localStorage.getItem('events');
+      if (savedEvents) {
+        const events: Event[] = JSON.parse(savedEvents);
+        this.eventDetails = events.find(e => e.id === eventId);
+        if (this.eventDetails) {
+          this.selectedExams = this.eventDetails.selectedExams;
+        }
+      }
     }
+  }
 
-    const savedEvents = localStorage.getItem('events');
-    if (savedEvents) {
-      const events: Event[] = JSON.parse(savedEvents);
-      this.event = events.find(e => e.id === eventId);
-      console.log('Found event:', this.event); // Debug log
-    }
-
-    if (!this.event) {
-      console.error('Event not found');
-      this.router.navigate(['/']);
-    }
+  goBack() {
+    this.router.navigate(['/']);
   }
 }
