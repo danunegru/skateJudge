064cc45ae0c53bc3311e401ea6e59dc4b119e633
vananhluong <luongvananh18@gmail.com>
2025-06-20 18:46:34 +0200

    feature: add exam component templates and routing

--- skate-judge-app/src/app/event-details-main/pruefungen/a1/a1.component.ts
+++ skate-judge-app/src/app/event-details-main/pruefungen/a1/a1.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { Event, Exam } from '../../../shared/models/event.interface';
 
 @Component({
   selector: 'app-a1',
@@ -11,12 +12,26 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class A1Component implements OnInit {
   eventId: string | null = null;
-  examId: string | null = null;
+  event: Event | undefined;
+  exam: Exam | undefined;
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.eventId = this.route.snapshot.paramMap.get('eventId');
-    this.examId = this.route.snapshot.paramMap.get('examId');
+    
+    if (this.eventId) {
+      // Get event from localStorage
+      const savedEvents = localStorage.getItem('events');
+      if (savedEvents) {
+        const events: Event[] = JSON.parse(savedEvents);
+        this.event = events.find(e => e.id === this.eventId);
+        
+        if (this.event) {
+          // Find the A1 exam in the event's selected exams
+          this.exam = this.event.selectedExams.find(e => e.id.toLowerCase() === 'a1');
+        }
+      }
+    }
   }
 }
--- skate-judge-app/src/app/event-details-main/pruefungen/a2/a2.component.ts
+++ skate-judge-app/src/app/event-details-main/pruefungen/a2/a2.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { Event, Exam } from '../../../shared/models/event.interface';
 
 @Component({
   selector: 'app-a2',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './a2.component.html',
   styleUrl: './a2.component.scss'
 })
-export class A2Component {
+
+export class A2Component implements OnInit {
+  eventId: string | null = null;
+  event: Event | undefined;
+  exam: Exam | undefined;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.eventId = this.route.snapshot.paramMap.get('eventId');
+    
+    if (this.eventId) {
+      // Get event from localStorage
+      const savedEvents = localStorage.getItem('events');
+      if (savedEvents) {
+        const events: Event[] = JSON.parse(savedEvents);
+        this.event = events.find(e => e.id === this.eventId);
+        
+        if (this.event) {
+          // Find the A2 exam in the event's selected exams
+          this.exam = this.event.selectedExams.find(e => e.id.toLowerCase() === 'a2');
+        }
+      }
+    }
+  }
 
 }
