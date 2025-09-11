79a9e602cd16584e34b292d6bd363a56571b49cc
mijhaoki <mij.baicu@icloud.com>
2025-09-12 01:53:17 +0200

    transfered from test project

--- skate-judge-app/src/app/sidebar/sidebar.component.ts
+++ skate-judge-app/src/app/sidebar/sidebar.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  standalone: true,
+  imports: [NgIf],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+  visible = true;
+  animating = false;
+  expanded = false;
 
+  hide() {
+    this.visible = false;
+    this.animating = true;
+  }
+
+  show() {
+    this.visible = true;
+    this.animating = true;
+  }
+
+  onTransitionEnd() {
+    if (!this.visible) {
+      this.animating = false;
+    }
+  }
+
+  toggleExpand() {
+    this.expanded = !this.expanded;
+  }
 }
