15bae91ce9829f24e0f40b6d9225605b3d114ad9
mijhaoki <mij.baicu@icloud.com>
2025-09-14 18:51:46 +0200

    refactored routes

--- skate-judge-app/src/app/app.component.ts
+++ skate-judge-app/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- skate-judge-app/src/app/home-page/sidebar/sidebar.component.ts
+++ skate-judge-app/src/app/home-page/sidebar/sidebar.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { NgIf } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf, RouterLink],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
